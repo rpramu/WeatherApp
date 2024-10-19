c235be90b4ddd1d7e92d30b33e6a973d43836df8
rpramu <pavan2762001@gmail.com>
2024-10-19 12:22:21 +0530

    Dummy

--- router.py
+++ router.py
@@ -28,25 +28,6 @@ def read_weather(city: str):
 
 
 
-
-
-@app.get("/weatherof/{city}/{units}")
-def read_weather_with_units(city: str, units: str):
-    API_KEY = os.getenv("OPEN_WEATHERMAP_API_KEY")
-    weather_data = get_weather_data(city, API_KEY, units=units)
-    if weather_data:
-        return {
-            "city": city,
-            "main": weather_data['main'],
-            "temperature": weather_data['temp'],
-            "feels_like": weather_data['feels_like'],
-            "humidity": weather_data['humidity']
-        }
-    else:
-        raise HTTPException(status_code=404, detail="Weather data not found")
-
-
-
 @app.get("/weatherof/{city}/{units}")
 def read_weather_with_units(city: str, units: str):
     API_KEY = os.getenv("OPEN_WEATHERMAP_API_KEY")
