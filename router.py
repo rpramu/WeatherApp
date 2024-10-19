da39ef8f28aabec4f70fbbefe2c975382b8f45ee
rpramu <pavan2762001@gmail.com>
2024-10-19 12:20:23 +0530

    Sample commit

--- router.py
+++ router.py
@@ -47,6 +47,22 @@ def read_weather_with_units(city: str, units: str):
 
 
 
+@app.get("/weatherof/{city}/{units}")
+def read_weather_with_units(city: str, units: str):
+    API_KEY = os.getenv("OPEN_WEATHERMAP_API_KEY")
+    weather_data = get_weather_data(city, API_KEY, units=units)
+    if weather_data:
+        return {
+            "city": city,
+            "main": weather_data['main'],
+            "temperature": weather_data['temp'],
+            "feels_like": weather_data['feels_like'],
+            "humidity": weather_data['humidity']
+        }
+    else:
+        raise HTTPException(status_code=404, detail="Weather data not found")
+
+
 
 
 
